b5d0c50ae5ff5c82f04f616bfadd8907aa380cdd
akash4952 <badola4952@gmail.com>
2021-12-17 17:43:49 +0530

    hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,8 +27,11 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{
-    scrollPositionRestoration:'enabled'
+    scrollPositionRestoration:'enabled',useHash:true
   })],
+  
   exports: [RouterModule]
+  
 })
+
 export class AppRoutingModule { }
